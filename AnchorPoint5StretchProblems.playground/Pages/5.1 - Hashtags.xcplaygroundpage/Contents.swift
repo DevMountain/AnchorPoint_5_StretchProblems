b470daa67afed9b02ae116ba7a9af0856c48fc74
Cameron Stuart <cameron@cameronstuart.com>
2020-10-05 06:56:20 -0400

    Adds description to the model.

--- AnchorPoint5StretchProblems.playground/Pages/5.1 - Hashtags.xcplaygroundpage/Contents.swift
+++ AnchorPoint5StretchProblems.playground/Pages/5.1 - Hashtags.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
  # Monday Stretch Problem 5.1
  ## Hashtags
  ### Instructions - Hashtags
- - Build a `Restaurant` model object with a `name`, `streetAddress`, `city`, `state`, and `reviews` properties.
+ - Build a `Restaurant` model object with a `name`, `description`, `streetAddress`, `city`, `state`, and `reviews` properties.
  - Build a `Review` model object with a `username` and `text` property.
  - Instantiate an array of `Restaurant` and `Review` objects. Make sure that each `Restaurant` object holds an array of reviews. Add hashtags to some of your reviews or restaruant descriptions. (Use Yelp or Google to help you come up with `Restaurants` and `Reviews`).
  - Add a `hashtags` computed property to the `Restaurant` object that returns an array of any hashtags in the `Restaurant` or `Review`.
